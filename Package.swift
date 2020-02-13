06dfc672ffa7fc35188dbaab2a546fde92e9e6c7
tomoki_sun <tomoki69386@gmail.com>
2020-02-14 05:31:21 +0900

    Update Package

--- Package.swift
+++ Package.swift
@@ -6,20 +6,12 @@ import PackageDescription
 let package = Package(
     name: "PKHUD",
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "PKHUD",
-            targets: ["PKHUD"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .library(name: "PKHUD", targets: ["PKHUD"])
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "PKHUD",
-            dependencies: []),
+            path: "PKHUD"
+        )
     ]
 )
